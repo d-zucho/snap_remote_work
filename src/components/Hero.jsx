646b324e969933a28d8bd00852455980b436b1ca
D_Zucho <danny.nico.dev@outlook.com>
2023-06-24 15:02:35 -0700

    add hero section and start styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,22 @@
+import HeroImage from '../assets/hero-desktop.png'
+import './styles/hero.styles.css'
+
 const Hero = () => {
   return (
-    <div>
-      <h1>
-        <span className='title-first'>Make</span> remote work
-      </h1>
-      <p className='body-copy'>
-        Get your team in sinc, no matter your location, Streamline processes,
-        create team rituals, and watch prpductivity soar.
-      </p>
+    <div className='hero-container'>
+      <div className='hero-left'>
+        <h1 className='title'>
+          <span className='title-first'>Make</span> remote work
+        </h1>
+        <p className='body-copy'>
+          Get your team in sinc, no matter your location, Streamline processes,
+          create team rituals, and watch prpductivity soar.
+        </p>
+        <button className='hero-cta'>Learn more</button>
+      </div>
+      <div className='hero-right'>
+        <img src={HeroImage} alt='hero' className='hero-image' />
+      </div>
     </div>
   )
 }
